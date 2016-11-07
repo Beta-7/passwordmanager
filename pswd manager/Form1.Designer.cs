859b6d988ee13d6e48cb1ceb06137709ae6a67f7
David <David@DAVID-PC>
2016-11-07 21:05:24 +0100

    Add project files.

---
+++ pswd manager/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace pswd_manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Корисничко име";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(140, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Лозинка";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Продолжи";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 185);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Нов Корисник?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(140, 38);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "Лозинка";
+            this.textBox3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 214);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ pswd manager/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Data.SQLite;
+
+namespace pswd_manager
+{
+
+    
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            System.IO.Directory.CreateDirectory(appdatafolder());
+            //najdi go %appdata%  folderot
+
+            textBox1.Text = Cryptography.Encrypt("david", "asd");
+            textBox2.Text = Cryptography.Decrypt(textBox1.Text, "asd");
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox3.Visible = checkBox1.Checked;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked && textBox2.Text == textBox3.Text && textBox1.Text!="" && textBox2.Text!="")
+            {
+                string databasefajl = appdatafolder() +"\\"+ textBox1.Text + ".sqlite";
+                string enkriptirandavid = Cryptography.Encrypt("david", textBox2.Text);
+                //enkriptiraj "david" so passwordot na fajlot
+                SQLiteConnection.CreateFile(databasefajl);
+                //kreiraj fajl so ime username.sqlite sto se naoga vo appdata folderot
+                SQLiteConnection dbConnection;
+                dbConnection =
+                new SQLiteConnection("Data Source="+databasefajl+";Version=3;");
+                dbConnection.Open();
+                string komanda = "create table passwords (id integer primary key autoincrement,URL varchar(150), name varchar(150), username varchar(150), password varchar(150), notes varchar(1500))";
+                //kreiraj tabela kade sto ke se skladiraat podatocite za veb stranite
+                SQLiteCommand izvrsikomanda = new SQLiteCommand(komanda, dbConnection);
+                izvrsikomanda.ExecuteNonQuery();
+                string komanda2 = "insert into passwords (url, name) values ('"+enkriptirandavid+ "', '" + enkriptirandavid + "')";
+                //vnesi david dva pati vo tabelata za podocna da se proveri dali passwordot e tocen
+                izvrsikomanda = new SQLiteCommand(komanda2, dbConnection);
+                izvrsikomanda.ExecuteNonQuery();
+
+            }
+        }
+        private string appdatafolder()
+        {
+            string AppData = Environment.ExpandEnvironmentVariables("%AppData%");
+
+            //pateka do %appdata%/common/passwordmanager folderot
+            string folder = AppData + "\\passwordmanager";
+            return folder;
+        }
+    }
+    public static class Cryptography
+    {
+        //lord have mercy on my soul
+        #region Settings
+
+        private static int _iterations = 2;
+        private static int _keySize = 256;
+
+        private static string _hash = "SHA1";
+        private static string _salt = "aselrias38490a32"; // Random
+        private static string _vector = "8947az34awl34kjq"; // Random
+
+        #endregion
+
+        public static string Encrypt(string value, string password)
+        {
+            return Encrypt<AesManaged>(value, password);
+        }
+        public static string Encrypt<T>(string value, string password)
+                where T : SymmetricAlgorithm, new()
+        {
+            byte[] vectorBytes = ASCIIEncoding.ASCII.GetBytes(_vector);
+            byte[] saltBytes = ASCIIEncoding.ASCII.GetBytes(_salt);
+            byte[] valueBytes = ASCIIEncoding.ASCII.GetBytes(value);
+
+            byte[] encrypted;
+            using (T cipher = new T())
+            {
+                PasswordDeriveBytes _passwordBytes =
+                    new PasswordDeriveBytes(password, saltBytes, _hash, _iterations);
+                byte[] keyBytes = _passwordBytes.GetBytes(_keySize / 8);
+
+                cipher.Mode = CipherMode.CBC;
+
+                using (ICryptoTransform encryptor = cipher.CreateEncryptor(keyBytes, vectorBytes))
+                {
+                    using (MemoryStream to = new MemoryStream())
+                    {
+                        using (CryptoStream writer = new CryptoStream(to, encryptor, CryptoStreamMode.Write))
+                        {
+                            writer.Write(valueBytes, 0, valueBytes.Length);
+                            writer.FlushFinalBlock();
+                            encrypted = to.ToArray();
+                        }
+                    }
+                }
+                cipher.Clear();
+            }
+            return Convert.ToBase64String(encrypted);
+        }
+
+        public static string Decrypt(string value, string password)
+        {
+            return Decrypt<AesManaged>(value, password);
+        }
+        public static string Decrypt<T>(string value, string password) where T : SymmetricAlgorithm, new()
+        {
+            byte[] vectorBytes = ASCIIEncoding.ASCII.GetBytes(_vector);
+            byte[] saltBytes = ASCIIEncoding.ASCII.GetBytes(_salt);
+            byte[] valueBytes = Convert.FromBase64String(value);
+
+            byte[] decrypted;
+            int decryptedByteCount = 0;
+
+            using (T cipher = new T())
+            {
+                PasswordDeriveBytes _passwordBytes = new PasswordDeriveBytes(password, saltBytes, _hash, _iterations);
+                byte[] keyBytes = _passwordBytes.GetBytes(_keySize / 8);
+
+                cipher.Mode = CipherMode.CBC;
+
+                try
+                {
+                    using (ICryptoTransform decryptor = cipher.CreateDecryptor(keyBytes, vectorBytes))
+                    {
+                        using (MemoryStream from = new MemoryStream(valueBytes))
+                        {
+                            using (CryptoStream reader = new CryptoStream(from, decryptor, CryptoStreamMode.Read))
+                            {
+                                decrypted = new byte[valueBytes.Length];
+                                decryptedByteCount = reader.Read(decrypted, 0, decrypted.Length);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return String.Empty;
+                }
+
+                cipher.Clear();
+            }
+            return Encoding.UTF8.GetString(decrypted, 0, decryptedByteCount);
+        }
+
+    }
+
+}

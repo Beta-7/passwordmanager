609a9d373f9b39d237eaa62040dc7e7cb5dd4cc5
David <David@DAVID-PC>
2017-01-13 21:52:53 +0100

    Obid za debagiranje na problemot so zakocuvanje na celata programa, komentiranje na del od kodot

--- pswd manager/mainform.cs
+++ pswd manager/mainform.cs
@@ -16,6 +16,7 @@ namespace pswd_manager
         public static string dbusername;
         public static string dbpassword;
         public static string dbnotes;
+        int maxid;
         public mainform()
         {
             InitializeComponent();
@@ -26,32 +27,52 @@ namespace pswd_manager
             login fasdorm1 = new login();
             masterusername = fasdorm1.getuser();
             masterpassword = fasdorm1.getpassword();
+            //zemi gi glavnite kredincijali od formata 1
 
             SQLiteConnection dbConnection;
             dbConnection =
             new SQLiteConnection("Data Source=" + fajl() + ";Version=3;");
             dbConnection.Open();
+            string idbroj ;          //promenliva za skladiranje na id brojot od posledniot rekord
+            string maxid = "SELECT LAST_INSERT_ID();";        //komanda za selektiranje na posledniot rekord
             string sql = "SELECT * FROM passwords ORDER BY id ";
+            SQLiteCommand maxidkomanda = new SQLiteCommand(sql, dbConnection);
+            SQLiteDataReader reader1 = maxidkomanda.ExecuteReader();
+            while (reader1.Read())
+            {
+                idbroj = reader1["id"].ToString();
+                MessageBox.Show("id=" + idbroj);
+                break;
+            }
             SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
             SQLiteDataReader reader = command.ExecuteReader();
             while (reader.Read())
             {
-                if (reader["id"].ToString() != "1")
+                if (reader["id"].ToString() != maxid)
                 {
-                    dbid = reader["id"].ToString();
-                    dburl = reader["URL"].ToString();
-                    dbname = reader["name"].ToString();
-                    dbusername = reader["username"].ToString();
-                    dbpassword = reader["password"].ToString();
-                    dbnotes = reader["notes"].ToString();
-                    dataGridView1.Rows.Add();
-                    dataGridView1.Rows[int.Parse(dbid) - 2].Cells[0].Value = (int.Parse(dbid) - 1).ToString();
-                    dataGridView1.Rows[int.Parse(dbid) - 2].Cells[1].Value = dburl;
-                    dataGridView1.Rows[int.Parse(dbid) - 2].Cells[2].Value = dbname;
-                    dataGridView1.Rows[int.Parse(dbid) - 2].Cells[3].Value = dbusername;
-                    dataGridView1.Rows[int.Parse(dbid) - 2].Cells[4].Value = dbpassword;
-                    dataGridView1.Rows[int.Parse(dbid) - 2].Cells[5].Value = dbnotes;
+                    if (reader["id"].ToString() != "1")
+                    {
+                        dbid = reader["id"].ToString();
+                        dburl = reader["URL"].ToString();
+                        dbname = reader["name"].ToString();
+                        dbusername = reader["username"].ToString();
+                        dbpassword = reader["password"].ToString();
+                        dbnotes = reader["notes"].ToString();
+                        dataGridView1.Rows.Add();
+                        dataGridView1.Rows[int.Parse(dbid) - 2].Cells[0].Value = (int.Parse(dbid) - 1).ToString();
+                        dataGridView1.Rows[int.Parse(dbid) - 2].Cells[1].Value = dburl;
+                        dataGridView1.Rows[int.Parse(dbid) - 2].Cells[2].Value = dbname;
+                        dataGridView1.Rows[int.Parse(dbid) - 2].Cells[3].Value = dbusername;
+                        dataGridView1.Rows[int.Parse(dbid) - 2].Cells[4].Value = dbpassword;
+                        dataGridView1.Rows[int.Parse(dbid) - 2].Cells[5].Value = dbnotes;
+                        MessageBox.Show(dbid);
 
+                    }
+               }
+                else
+                {
+                  MessageBox.Show("zatvoram konekcija");
+                    dbConnection.Close();
                 }
             }
 

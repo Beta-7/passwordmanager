82a1b108bdc3934530c0bce93cacf0eca03d1e81
MaliceGFS <MaliceGFS@users.noreply.github.com>
2017-07-09 21:57:15 -0500

    Misc changes in Program.cs

--- pswd manager/Program.cs
+++ pswd manager/Program.cs
@@ -3,15 +3,14 @@ using System.Windows.Forms;
 
 namespace pswd_manager
 {
-    public static class Program
+    public class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            //TODO: Add commandline argument functionality
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginForm());
